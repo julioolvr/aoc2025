f38ffc7675045055bd7b19cd4be8e9806ced6745
Julio <julio.olvr@gmail.com>
2025-12-06 16:26:32 +0100

    Day 05 Part 02

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    env,
+    cmp, env,
     fs::File,
     io::{self, BufRead},
     ops::RangeInclusive,
@@ -8,7 +8,7 @@ use std::{
 fn main() {
     let mut lines = read_lines().unwrap().map(|line| line.unwrap());
     let range_lines = lines.by_ref().take_while(|line| line != "");
-    let ranges: Vec<RangeInclusive<usize>> = range_lines
+    let mut ranges: Vec<RangeInclusive<usize>> = range_lines
         .map(|line| {
             let mut split = line.split('-');
             let from: usize = split.next().unwrap().parse().unwrap();
@@ -23,6 +23,29 @@ fn main() {
         .filter(|ingredient_id| ranges.iter().any(|r| r.contains(ingredient_id)))
         .count();
     println!("Part 1: {}", part_1);
+
+    ranges.sort_by_key(|range| *range.start());
+    let merged_ranges = ranges
+        .iter()
+        .skip(1)
+        .fold(vec![ranges[0].clone()], |mut acc, range| {
+            let last_range = acc.last().unwrap();
+            if last_range.end() >= range.start() {
+                let len = acc.len();
+                acc[len - 1] = (*last_range.start())..=(cmp::max(*range.end(), *last_range.end()));
+            } else {
+                acc.push(range.clone());
+            }
+
+            acc
+        });
+
+    let part_2: usize = merged_ranges
+        .iter()
+        .map(|range| range.end() - range.start() + 1)
+        .sum();
+
+    println!("Part 2: {}", part_2);
 }
 
 fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
